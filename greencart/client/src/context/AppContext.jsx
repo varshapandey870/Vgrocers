57e1674bf1f9660191fe1ab281fa7770beae02eb
Varsha Pandey <84341347+varshapandey870@users.noreply.github.com>
2025-08-05 20:17:39 +0530

    Update AppContext.jsx

--- greencart/client/src/context/AppContext.jsx
+++ greencart/client/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL =  "https://vgrocers-backend.onrender.com";
+axios.defaults.baseURL =  import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
